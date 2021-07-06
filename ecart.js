e25102a73c22b0fdcc6641f4472e8e67aa7ffc99
thennarasu882 <86654549+thennarasu882@users.noreply.github.com>
2021-07-06 17:19:09 +0530

    Added jQuery

--- ecart.js
+++ ecart.js
@@ -1,78 +1,91 @@
-const password = document.querySelector('#password');
-const togglePassword = document.querySelector('#togglePassword');
-const forgot = document.querySelector('.forgot');
-const use_pass = document.querySelector('#use_pass');
-const enter_otp = document.querySelector("#enter_otp");
-const mail = document.querySelector("#email");
-const warn_invalid_email = document.querySelector(".warn_invalid1");
-const warn_invalid_pass = document.querySelector(".warn_invalid2");
-const login = document.querySelector(".loginbtn");
-const or = document.querySelector('#or');
-
-togglePassword.addEventListener("click", function (e) {
-    const type = password.getAttribute("type") === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    const icon = this.getAttribute('class') === 'bi bi-eye' ? 'bi bi-eye-slash' : 'bi bi-eye';
-    this.setAttribute('class', icon);
-});
-
-login.addEventListener("click", function (e) {
-    if (mail.value.length == 0 && password.value.length == 0) {
-        alert("Please fill all the details")
-        e.preventDefault();
-    }
-    else {
-         otp.style.display = "none";
-        enter_otp.style.display = "none";
-        use_pass.style.display = "none";
-        or.style.display = "none";
-    }
-})
-const otp = document.querySelector('#req_otp');
-otp.addEventListener('click', function (e) {
-    password.style.display = "none";
-    forgot.style.display = "none";
-    togglePassword.style.display = "none";
-    otp.style.display = "none";
-    enter_otp.style.display = "block";
-    use_pass.style.display = "block";
-
-    e.preventDefault();
-});
-use_pass.addEventListener('click', function (e) {
-    password.style.display = "block";
-    forgot.style.display = "block";
-    forgot.style.transform = translateY(-30);
-    togglePassword.style.display = "block";
-    enter_otp.style.display = "none";
-    this.style.display = "none"
-    otp.style.display = "block";
-    e.preventDefault();
-});
-
-mail.addEventListener("input", validate);
-password.addEventListener("input", validate);
 const mail_regx = /^[\w._%+-]+@[\w.-]+\.[a-zA-z]{2,4}/;
 const mob_regx = /^[0-9]{10}/;
-const pass_regx = /^[\w._%+-@*#]{8,12}/
+const pass_regx = /^[\w._%+-@*#]{8}/;
+
+$(".warn_invalid1").hide();
+$(".warn_invalid2").hide();
+$("#enter_otp").hide();
+$("#use_pass").hide();
+
+$("#togglePassword").click(show_hide_pass);
+$(".loginbtn").click(validate);
+$("#email")[0].addEventListener("input", validate_mail);
+$("#email")[0].addEventListener("input", email_mobile);
+$("#password")[0].addEventListener("input", validate_pass);
+
+$("#req_otp").click(function (e) {
+  e.preventDefault();
+  $("#password").hide();
+  $(".forgot").hide();
+  $("#togglePassword").hide();
+  $("#req_otp").hide();
+  $("#enter_otp").show(100);
+  $("#use_pass").show();
+  $("#email").attr("placeholder", "Enter Mobile Number");
+});
+
+$("#use_pass").click(function (e) {
+  e.preventDefault();
+  $("#password").show(100);
+  $(".forgot").show();
+  $("#togglePassword").show();
+  $("#req_otp").show();
+  $("#enter_otp").hide();
+  $("#use_pass").hide();
+  $("#email").attr("placeholder", "Enter Email/ Mobile Number");
+});
+
+function show_hide_pass() {
+  const type = $("#password").attr("type") === "password" ? "text" : "password";
+  $("#password").attr("type", type);
+  const icon =
+    $(this).attr("class") === "bi bi-eye" ? "bi bi-eye-slash" : "bi bi-eye";
+  $(this).attr("class", icon);
+}
+
+function email_mobile() {
+  if (isNaN($("#email").val())) {
+    $("#email").attr("maxlength", "30");
+  } else {
+    $("#email").attr("maxlength", "10");
+  }
+}
+
+function validate_mail() {
+  if (
+    $("#email") &&
+    (mail_regx.test($("#email").val()) || mob_regx.test($("#email").val()))
+  ) {
+    $(".warn_invalid1").hide();
+    return true;
+  } else {
+    $(".warn_invalid1").show();
+    return false;
+  }
+}
+
+function validate_pass() {
+  if ($("#password").val() && pass_regx.test($("#password").val())) {
+    $(".warn_invalid2").hide();
+    return true;
+  } else {
+    $(".warn_invalid2").show();
+    return false;
+  }
+}
 
 function validate(e) {
-    let target = e.target;
-    if (target.name == "email") {
-        if (mail_regx.test(target.value) | mob_regx.test(target.value)) {
-             warn_invalid_email.style.display = "none";
-        }
-        else {
-             warn_invalid_email.style.display = "block";
-        }
+  e.preventDefault();
+  if ($("#email").val() && $("#password").val()) {
+    if (validate_mail() && validate_pass()) {
+      $("#req_otp").hide();
+      $("#enter_otp").hide();
+      $("#use_pass").hide();
+      $("#or").hide();
+    } else {
+      alert("Provided invalid data...check once again and fill it correctly");
     }
-
-    if (target.name == "password") {
-        if (pass_regx.test(target.value)){
-             warn_invalid_pass.style.display = "none";
-        }
-        else {
-             warn_invalid_pass.style.display = "block";
-        }
-    }
+  } else {
+    alert("Please fill all the details");
+  }
 }
